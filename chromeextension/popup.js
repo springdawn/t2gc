fc37306b6343c5a60eea5e99abd84722118f1952
Takashi Okano <honiho@springdawn.info>
2013-08-31 04:03:56 +0900

    change not to going through background page to get selection

--- chromeextension/popup.js
+++ chromeextension/popup.js
@@ -1,8 +1,26 @@
 console.log('popup');
-chrome.runtime.getBackgroundPage(function(background) {
-    background.getSelect(function(data){
-        console.log(data);
-        document.getElementById('data').innerHTML = data;
-        $.get('http://localhost:8000', {'selectionText': ''});
-    });
+//chrome.runtime.getBackgroundPage(function(background) {
+//    background.getSelect(function(data){
+//        console.log(data);
+//        document.getElementById('data').innerHTML = data;
+//        $.get('http://localhost:8000', {'selectionText': ''});
+//    });
+//});
+
+getSelect(function(data){
+    console.log(data);
+    document.getElementById('data').innerHTML = data;
+    $.get('http://localhost:8000', {'selectionText': ''});
 });
+
+function getSelect(done) {
+    popupData = '';
+    chrome.tabs.query({active:true}, function(tabs) {
+        console.log(tabs[0].url);
+        chrome.tabs.sendMessage(tabs[0].id, {request:true}, function(response) {
+            console.log(response);
+            popupData = response.select || 'none';
+            done(popupData);
+        });
+    });
+}
--- chromeextension/t2gc.js
+++ chromeextension/t2gc.js
@@ -7,14 +7,14 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({title:'t2gc', 'id':'t2gc', contexts:['selection']});
 });
 chrome.contextMenus.onClicked.addListener(onClickedHandler);
-function getSelect(done) {
-    popupData = '';
-    chrome.tabs.query({active:true}, function(tabs) {
-        console.log(tabs[0].url);
-        chrome.tabs.sendMessage(tabs[0].id, {request:true}, function(response) {
-            console.log(response);
-            popupData = response.select || 'none';
-            done(popupData);
-        });
-    });
-}
+//function getSelect(done) {
+//    popupData = '';
+//    chrome.tabs.query({active:true}, function(tabs) {
+//        console.log(tabs[0].url);
+//        chrome.tabs.sendMessage(tabs[0].id, {request:true}, function(response) {
+//            console.log(response);
+//            popupData = response.select || 'none';
+//            done(popupData);
+//        });
+//    });
+//}
