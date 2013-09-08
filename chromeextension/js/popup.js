0267af594015bed5721844b71afb8cc851dc09b3
Takashi Okano <honiho@springdawn.info>
2013-09-09 05:22:01 +0900

    add function to insert event from Chrome extension

--- chromeextension/js/popup.js
+++ chromeextension/js/popup.js
@@ -10,7 +10,7 @@ console.log('popup');
 getSelect(function(data){
     console.log(data);
     document.getElementById('data').innerHTML = data;
-    $.get('http://localhost:8000', {'selectionText': ''});
+//    $.get('http://localhost:8000', {'selectionText': ''});
 });
 
 //chrome.notifications.create('', {title:'ABC', message:'defg', type:'basic', iconUrl:'../dummy.jpg'}, function(id){console.log('notification id:', id);});
@@ -30,7 +30,53 @@ function getSelect(done) {
 $(function() {
     $('#date').datepicker({dateFormat: 'yy-mm-dd'});
     $('#start_time').timepicker({stepMinute: 5, hourGrid:3, minuteGrid:15});
-    $('#end_time').timepicker({stepMinute: 5});
+    $('#end_time').timepicker({stepMinute: 5, hourGrid:3, minuteGrid:15});
+    $('#submit').prop('disabled', true);
+    $('#date').change(function(){
+        if($('#date').val()) {
+            $('#submit').prop('disabled', false);
+        } else {
+            $('#submit').prop('disabled', true);
+        }
+    });
+    $('#submit').click(submit);
 //    $('#date').datepicker('setDate', '2013-09-01');
     //$('#date').datepicker();
 });
+
+function submit(e) {
+    console.log("submit")
+    var summary = encodeURI($('#summary').val()) || '';
+    var dateval = $('#date').val();
+    var date = validate('date', dateval)? dateval: '';
+    var start_timeval = $('#start_time').val();
+    var start_time = validate('time', start_timeval)? start_timeval: '';
+    var end_timeval = $('#end_time').val();
+    var end_time = validate('time', end_timeval)? end_timeval: '';
+    var location = encodeURI($('#location').val()) || '';
+    if(!date || (!start_time && end_time)) {
+        console.log("invalid data");
+        $("#status").html("invalid data");
+        return;
+    }
+    $.get('http://localhost:8000',
+          {title: summary, date: date, start: start_time, end: end_time, location: location},
+          function(data){
+              console.log(data)
+          });
+}
+
+function validate(type, val) {
+    if (!val) return false;
+    if(type == 'date') {
+        try {
+            $.datepicker.parseDate($.datepicker.W3C, val);
+            return true;
+        } catch(e) {
+            return false;
+        }
+    } else if(type == 'time') {
+        var reg = /^(?:[01]\d|2[0-3]):[0-5]\d$/;
+        return reg.test(val);
+    }
+}
--- server.py
+++ server.py
@@ -1,14 +1,52 @@
+# This Python file uses the following encoding: utf-8
+
+from ns import *
 import SimpleHTTPServer
-from urlparse import urlparse
+from urlparse import urlparse, parse_qsl
 import urllib2
+from googleCalendar import GoogleCalendar
+jst = '+0900'
 
 
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         getpath = urlparse(self.path)
-        print urllib2.unquote(getpath.query)
+        unquotedquery = urllib2.unquote(getpath.query)
+        querydict = dict(parse_qsl(unquotedquery))
+        datestring = querydict['date'] if 'date' in querydict else ''
+        if not datestring:
+            self.send_error(400, 'date is required')
+            return
+        start_time = ''
+        if 'start' in querydict:
+            start_time = datestring + 'T' + querydict['start'] + ':00' + jst
+        end_time = ''
+        if start_time and 'end' in querydict:
+            end_time = datestring + 'T' + querydict['end'] + ':00' + jst
+        elif start_time:
+            end_time = (start_time[:11] +
+                        str(int(start_time[11:13]) + 1).zfill(2) +
+                        start_time[13:])
+        req = {
+            'title': querydict['title'] if 'title' in querydict else None,
+            'location': querydict['location']
+            if 'location' in querydict else None,
+            'start': start_time if start_time else None,
+            'end': end_time if end_time else None,
+            'date': datestring if not start_time and not end_time else None
+        }
+        res = gc.createEvent(req)
+        if not res:
+            res = gc.createEvent(req)
+        self.wfile.write('ok ' + res)
 
-httpd = SimpleHTTPServer.BaseHTTPServer.HTTPServer(
-    ('localhost', 8000), GetHandler)
-print httpd.server_name, httpd.server_port
-httpd.serve_forever()
+
+def createServer():
+    httpd = SimpleHTTPServer.BaseHTTPServer.HTTPServer(
+        ('localhost', 8000), GetHandler)
+    print httpd.server_name, httpd.server_port
+    httpd.serve_forever()
+
+gc = GoogleCalendar()
+gc.connect()
+createServer()
